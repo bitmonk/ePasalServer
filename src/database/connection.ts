fb79c97dee44473eb46bd41f0dc3317c4f109260
Parbat Tamang <parbattamang1234@gmail.com>
2024-11-17 17:53:26 +0545

    table creation...

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,7 +1,9 @@
 import { Sequelize } from "sequelize-typescript";
 import envConfig from "../config/config";
 
-const sequelize = new Sequelize(envConfig.connectionString as string)
+const sequelize = new Sequelize(envConfig.connectionString as string, {
+    models: [__dirname + '/models']
+})
 
 try {
     sequelize.authenticate()
@@ -14,5 +16,9 @@ try {
 } catch(error) {
     console.log(error);
 }
+
+sequelize.sync({force : false, alter : true}).then(()=> {
+    console.log('synced');
+})
 
 export default sequelize
